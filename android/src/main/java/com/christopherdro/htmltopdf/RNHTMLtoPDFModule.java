4e6eae14b0b0bf747ce70856fcbbe597bc755b20
ARRABIDA\bruno.oliveira <bruno.oliveira@finantech.pt>
2019-04-17 17:41:12 +0100

    - Changed directory to Downloads on Android instead of Documents

--- android/src/main/java/com/christopherdro/htmltopdf/RNHTMLtoPDFModule.java
+++ android/src/main/java/com/christopherdro/htmltopdf/RNHTMLtoPDFModule.java
@@ -45,7 +45,7 @@ public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
       if (options.hasKey("directory") && options.getString("directory").equals("docs")) {
         String state = Environment.getExternalStorageState();
           File path = (Environment.MEDIA_MOUNTED.equals(state)) ?
-                  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+                  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
                   : new File(mReactContext.getFilesDir(), Environment.DIRECTORY_DOCUMENTS);
 
         if (!path.exists()) path.mkdir();
